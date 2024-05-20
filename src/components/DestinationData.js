99235d7e8c2614b3344b08f8d3f73279bbb6edc6
tenzin-chime <tenchime27@gmail.com>
2024-05-20 17:11:49 +0530

    dynamic destination

--- src/components/DestinationData.js
+++ src/components/DestinationData.js
@@ -1,11 +1,23 @@
 import { Component } from "react";
 import "./DestinationStyles.css";
 
+
 class DestinationData extends Component {
   render() {
     return (
-      <div>
-        <h1>Your Code Here</h1>
+      <div className={this.props.className}>
+
+      <div className="des-text">
+        <h2>{this.props.heading}</h2>
+        <p>{this.props.text}</p>
+      </div>
+  
+      <div className="image">
+        <img alt="img" src={this.props.image1} />
+        <img alt="img" src={this.props.image2} />
+      </div>
+      
+  
       </div>
     );
   }
